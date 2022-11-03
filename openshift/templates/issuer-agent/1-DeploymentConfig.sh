150546cc9436af942f16592a4d190bf493ab0e01
Philippe Foucault <philippe.foucault@mcn.gouv.qc.ca>
2022-11-03 15:20:57 -0400

    Ajuster variable de la BD
    Signed-off-by: Philippe Foucault <philippe.foucault@mcn.gouv.qc.ca>

--- openshift/templates/issuer-agent/1-DeploymentConfig.sh
+++ openshift/templates/issuer-agent/1-DeploymentConfig.sh
@@ -1,3 +1,3 @@
-oc import-image bcgovimages/aries-cloudagent:py36-1.16-1_0.7.5 --confirm --scheduled=true
+# oc import-image bcgovimages/aries-cloudagent:py36-1.16-1_0.7.5 --confirm --scheduled=true
 oc process -f issuer-agent-deploy.yaml --param-file=issuer-agent-deploy.port-e.env | oc apply -f -
 # oc process -f 6-DeploymentConfig.yaml --param-file=6-DeploymentConfig.env
