c69c27daef1a5bc9d9e292fa9d90da0daf4a392e
Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>
2022-11-20 19:24:25 -0500

    [func] creation de templates pour le controlleur
    Signed-off-by: Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>

---
+++ openshift/templates/issuer-controller/0-CreateProject.sh
@@ -0,0 +1,1 @@
+oc new-project exp-port-e-agent-torjc01 --display-name "Experimentation Port-e (Agent)" --description="Expérimentation du portefeuille en tant qu'employé (Agent)"
---
+++ openshift/templates/issuer-controller/1-DeploymentConfig.sh
@@ -0,0 +1,1 @@
+oc process -f aries-issuer-controller.yaml --param-file=aries-issuer-controller.candy-dev.params | oc apply -f -
