e37b07e598871584a4ddc1bbec786deda3d5e8e0
Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>
2022-11-20 19:23:15 -0500

    [func] renomme le repertoire
    Signed-off-by: Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>

--- openshift/templates/isser-controller/0-CreateProject.sh
+++
@@ -1,1 +0,0 @@
-oc new-project exp-port-e-agent --display-name "Experimentation Port-e (Agent)" --description="Expérimentation du portefeuille en tant qu'employé (Agent)"
--- openshift/templates/isser-controller/1-DeploymentConfig.sh
+++
@@ -1,1 +0,0 @@
-oc process -f aries-issuer-controller.yaml --param-file=aries-issuer-controller.candy-dev.env | oc apply -f -
