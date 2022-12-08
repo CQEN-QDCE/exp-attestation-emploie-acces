ffeaf962f8a4b6f2ed40e6790a34362cc0ad7c56
Philippe Foucault <philippe.foucault@mcn.gouv.qc.ca>
2022-12-08 12:01:30 -0500

    bonification des labels pour keycloak
    Signed-off-by: Philippe Foucault <philippe.foucault@mcn.gouv.qc.ca>

--- openshift/templates/consumer-vcauthn/1-DeploymentConfig.sh
+++ openshift/templates/consumer-vcauthn/1-DeploymentConfig.sh
@@ -1,1 +1,2 @@
-oc process -f keycloak.yaml --param-file=keycloak.dev.params | oc apply -f -
+oc process -f keycloak.yaml --param-file=keycloak.dev.env | oc apply -f -
+# oc process -f vcauthn.yaml --param-file=vcauthn.dev.env | oc apply -f -
