ce65c6edc2bd310011f23f233e81e6b82a8f7722
Philippe Foucault <philippe.foucault@mcn.gouv.qc.ca>
2022-12-09 11:42:37 -0500

    ajouter l'agent consommateur connection-less
    Signed-off-by: Philippe Foucault <philippe.foucault@mcn.gouv.qc.ca>

--- openshift/templates/consumer-vcauthn/1-DeploymentConfig.sh
+++ openshift/templates/consumer-vcauthn/1-DeploymentConfig.sh
@@ -1,2 +1,2 @@
-oc process -f keycloak.yaml --param-file=keycloak.dev.env | oc apply -f -
-# oc process -f vcauthn.yaml --param-file=vcauthn.dev.env | oc apply -f -
+# oc process -f keycloak.yaml --param-file=keycloak.dev.params.env | oc apply -f -
+oc process -f vcauthn.yaml --param-file=vcauthn.dev.params.env | oc apply -f -
